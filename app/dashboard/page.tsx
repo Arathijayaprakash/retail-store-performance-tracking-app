f3a2f26afa06d3924be7a278d43b25e26ec1d4f5
Arathi P <arathi.jayaprakash@aspiresys.kl>
2025-10-27 12:14:59 +0530

    first commit-organized folder sturcture

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+import { lusitana } from "../ui/fonts";
+
+export default async function Page() {
+    return (
+        <main>
+            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+                Dashboard
+            </h1>
+            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+                <div>Top performer</div>
+            </div>
+            <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+                <div>Chart</div>
+            </div>
+        </main>
+    );
+}
